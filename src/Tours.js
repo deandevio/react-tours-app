e1212b98c542f11098dc2c597db4dc745c0bed71
deandev <iamdekanos@gmail.com>
2022-01-20 21:01:35 +0200

    Some props drilling before diving into context api in the next project

--- src/Tours.js
+++ src/Tours.js
@@ -1,6 +1,21 @@
 import Tour from "./Tour";
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = (props) => {
+  const { tours } = props;
+
+  return (
+    <section>
+      <div className="title">
+        <h2>All Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour id={tour.id} image={tour.image} info={tour.info} price={tour.price} name={tour.name} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
